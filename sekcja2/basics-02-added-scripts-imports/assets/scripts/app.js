3bf16068e0a05363e01e5092fa465efd8c59366f
Aleksandra <akrysinska@emyad.com>
2022-04-21 08:27:59 +0200

    More about return statement

--- sekcja2/basics-02-added-scripts-imports/assets/scripts/app.js
+++ sekcja2/basics-02-added-scripts-imports/assets/scripts/app.js
@@ -1,6 +1,13 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function add(num1, num2) {
+    const result = num1 + num2;
+    return result;
+}
+
+currentResult = add(1, 2);
+
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
 let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`;
