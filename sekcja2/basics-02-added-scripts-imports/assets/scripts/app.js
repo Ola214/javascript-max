edcfe45fcf038dc6dadd8769f80a35db06a43ade
Aleksandra <akrysinska@emyad.com>
2022-04-21 09:38:43 +0200

    using arrays

--- sekcja2/basics-02-added-scripts-imports/assets/scripts/app.js
+++ sekcja2/basics-02-added-scripts-imports/assets/scripts/app.js
@@ -1,5 +1,11 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
+
+//This is a comment!
+/*
+    This is also a comment!
+*/
 
 function getUserNumberInput() {
     return parseInt(usrInput.value);
@@ -7,34 +13,36 @@ function getUserNumberInput() {
 
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
     const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-    outputResult(currentResult, calcDescription);
+    outputResult(currentResult, calcDescription); //from vendor file
 }
 
 function add() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult + enteredNumber;
+    currentResult += enteredNumber;
     createAndWriteOutput('+', initialResult, enteredNumber); 
+    logEntries.push(enteredNumber);
+    console.log(logEntries[0]);
 }
 
 function subtract() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult - enteredNumber;
+    currentResult -=  enteredNumber;
     createAndWriteOutput('-', initialResult, enteredNumber); 
 }
 
 function multiply() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult * enteredNumber;
+    currentResult *=  enteredNumber;
     createAndWriteOutput('*', initialResult, enteredNumber);
 }
 
 function divide() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult / enteredNumber;
+    currentResult /= enteredNumber;
     createAndWriteOutput('*', initialResult, enteredNumber);
 }
 
