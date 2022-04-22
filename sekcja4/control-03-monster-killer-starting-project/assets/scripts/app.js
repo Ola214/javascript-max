4b1469c8753ef764fac85b51d8b3ade24f9a43e9
Aleksandra <akrysinska@emyad.com>
2022-04-22 10:07:05 +0200

    do while loop

--- sekcja4/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ sekcja4/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -140,6 +140,13 @@ function printLogHandler() {
     // for (let i = 0; i < battleLog.length; i++) {
     //     console.log('---------------');
     // }
+
+    let j = 0;
+    do {
+        console.log(j);
+        j++;
+    } while (j < 3);
+
     let i = 0;
     for (const logEntry of battleLog) {
         console.log(`#${i}`);
