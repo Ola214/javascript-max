09ddcc6059226887fe0a3a2c294c64ff8c26eb81
Aleksandra <akrysinska@emyad.com>
2022-04-22 11:57:41 +0200

    cd

--- sekcja5/behind-scenes-01-starting-setup/app.js
+++ sekcja5/behind-scenes-01-starting-setup/app.js
@@ -1,14 +1,21 @@
-let name = 'Max';
+// let name = 'Max';
 
-if (name === 'Max') {
-    var hobbies = ['Sports', 'Cooking'];
-    console.log(hobbies);
-}
+// if (name === 'Max') {
+//     var hobbies = ['Sports', 'Cooking'];
+//     console.log(hobbies);
+// }
 
-function greet() {
-    let age = 30;
-    let name = 'Manuel';
-    console.log(name, age);
-}
-console.log(name, hobbies);
-greet();
+// function greet() {
+//     let age = 30;
+//     let name = 'Manuel';
+//     console.log(name, age);
+// }
+// console.log(name, hobbies);
+// greet();
+
+'use strict';
+
+const userName = 'Max';
+var undefined = 5;
+
+console.log(useName);
