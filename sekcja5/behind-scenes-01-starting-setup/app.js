64d29151cabe702b4d4eaf6dd6517a3368347826
Aleksandra <akrysinska@emyad.com>
2022-04-22 11:32:24 +0200

    var vs let & const

---
+++ sekcja5/behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,14 @@
+let name = 'Max';
+
+if (name === 'Max') {
+    var hobbies = ['Sports', 'Cooking'];
+    console.log(hobbies);
+}
+
+function greet() {
+    let age = 30;
+    let name = 'Manuel';
+    console.log(name, age);
+}
+console.log(name, hobbies);
+greet();
